3613e4444d5dfd35f53371574ed86cd4cb7c4931
Udhaya Shankar J <220701306@rajalakshmi.edu.in>
2025-04-11 13:30:11 -0400

    Exp8

---
+++ Exp8/y.tab.h
@@ -0,0 +1,22 @@
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
+
+#define NUMBER 257
+#define TYPE 258
+#define ID 259
+#define MAIN 260
+#ifdef YYSTYPE
+#undef  YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+#endif
+#ifndef YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+typedef union YYSTYPE{
+	int ival;
+	double dval;
+	char string[10];
+} YYSTYPE;
+#endif /* !YYSTYPE_IS_DECLARED */
+extern YYSTYPE yylval;
+
+#endif /* _yy_defines_h_ */
